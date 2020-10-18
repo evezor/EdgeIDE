0b5164b30b40f70f33e6c92744b569eb8d7663ae
jcmertz <joe@joemertz.com>
2020-10-18 01:51:08 -0500

    minor feature add: saving parameter table now closes form

--- app/paramHandler.js
+++ app/paramHandler.js
@@ -1,4 +1,5 @@
-function paramHandler(form){
+function paramHandler(form) {
   var test = form.name.value;
   app.view.getFigure(form.figureID.value).getChildren().get(0).setText(form.name.value);
+  $(form).closest(".ui-dialog-content").dialog("close");
 }
