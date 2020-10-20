2d6e863a15e23c16ccf86a3b2f2a4defc33413ff
jcmertz <joe@joemertz.com>
2020-10-20 03:11:03 -0500

    Make Label Attributes Perisitent. This commit brought to you by Club Mate and Sailor Jerry

--- app/B3.js
+++ app/B3.js
@@ -13,42 +13,74 @@ B3 = draw2d.shape.basic.Rectangle.extend({
       bgColor: "#f7f7f7",
       radius: this.getRadius(),
       padding: 10,
-      resizeable: true,
-      //editor: new draw2d.ui.LabelInplaceEditor()
+      resizeable: false,
     });
     this.add(this.classLabel, new draw2d.layout.locator.CenterLocator());
 
-		/** Init IO
-		*  This section is used to initialize the required IO for a board
-		*
-		**/
-		var led0 = this.createPort("input");
-		led0.add(new draw2d.shape.basic.Label({text: "LED 0",}),new draw2d.layout.locator.RightLocator());
-		var led1 = this.createPort("input");
-		led1.add(new draw2d.shape.basic.Label({text: "LED 1",}),new draw2d.layout.locator.RightLocator());
-		var button0 = this.createPort("output");
-		button0.add(new draw2d.shape.basic.Label({text: "Button 0",}),new draw2d.layout.locator.LeftLocator());
-		var button1 = this.createPort("output");
-		button1.add(new draw2d.shape.basic.Label({text: "Button 1",}),new draw2d.layout.locator.LeftLocator());
+    /** Init IO
+     *  This section is used to initialize the required IO for a board
+     *
+     **/
+    var led0 = this.createPort("input");
+    led0.add(new draw2d.shape.basic.Label({
+      text: "LED 0",
+    }), new draw2d.layout.locator.RightLocator());
+    var led1 = this.createPort("input");
+    led1.add(new draw2d.shape.basic.Label({
+      text: "LED 1",
+    }), new draw2d.layout.locator.RightLocator());
+    var button0 = this.createPort("output");
+    button0.add(new draw2d.shape.basic.Label({
+      text: "Button 0",
+    }), new draw2d.layout.locator.LeftLocator());
+    var button1 = this.createPort("output");
+    button1.add(new draw2d.shape.basic.Label({
+      text: "Button 1",
+    }), new draw2d.layout.locator.LeftLocator());
 
-		var pot0 = this.createPort("output");
-		pot0.add(new draw2d.shape.basic.Label({text: "Pot 0",}),new draw2d.layout.locator.LeftLocator());
+    var pot0 = this.createPort("output");
+    pot0.add(new draw2d.shape.basic.Label({
+      text: "Pot 0",
+    }), new draw2d.layout.locator.LeftLocator());
+  },
 
-  } ,
 
+  /**
+   * @method
+   * Set the name of the DB table. Visually it is the header of the shape
+   *
+   * @param name
+   */
+  setName: function(name) {
+    this.classLabel.setText(name);
 
-   /**
-    * @method
-    * Set the name of the DB table. Visually it is the header of the shape
-    *
-    * @param name
-    */
-   setName: function(name)
-   {
-       this.classLabel.setText(name);
+    return this;
+  },
 
-       return this;
-   }
+  /**
+   * @method
+   *  returns all attributes for JSON serialization
+   *
+   * @param none
+   */
+  getPersistentAttributes: function() {
+    var memento = {
+      label: this.classLabel.getPersistentAttributes(),
+      primary: this._super()
+    }
+    return memento;
+  },
+
+  /**
+   * @method
+   *  read all attributes for JSON serialization
+   *
+   * @param memento
+   */
+  setPersistentAttributes: function(memento) {
+    this.classLabel.setPersistentAttributes(memento.label);
+    this._super(memento.primary);
+  }
 
 
 
