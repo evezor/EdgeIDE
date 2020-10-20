00a0f3a5ad7ebd834991ab0b3d7f889040d9b41a
jcmertz <joe@joemertz.com>
2020-10-20 01:37:03 -0500

    minor change for clarity

--- app/evezor.js
+++ app/evezor.js
@@ -1,6 +1,6 @@
 function paramHandler(form) {
   var test = form.name.value;
-  app.view.getFigure(form.figureID.value).getChildren().get(0).setText(form.name.value);
+  app.view.getFigure(form.figureID.value).setName(form.name.value);
   $(form).closest(".ui-dialog-content").dialog("close");
 }
 
